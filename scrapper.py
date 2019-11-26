5a2687bbfc2bbddd2eb9c46a3e04127a55428b57
talimur <42349848+talimur@users.noreply.github.com>
2019-11-26 12:31:41 -0300

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,47 +1,46 @@
 import requests
 import lxml
+import pprint
 from bs4 import BeautifulSoup
-import pandas as pd
+#import pandas as pd
 
 # site a ser iterado https://sistemas.anac.gov.br/certificacao/AvGeral/AIR145BasesDetail.asp?B145Codi=0000000001 to 0000001500
 
 
-i = 1
+def webscraper():
+  i = 1
+  x = 0
+  dicio_fornecedores = {}
+    #roda atraves dos numeros de fornecedor
+  for i in range(i,3):
+    
+    pagina = requests.get(("https://sistemas.anac.gov.br/certificacao/AvGeral/AIR145BasesDetail.asp?B145Codi=000000"+ str('%04d'%i)))
+    print (i)
+    #transforma página em objeto
+    soup = BeautifulSoup(pagina.text, 'lxml')
 
-#roda atraves dos numeros de fornecedor
-for i in range(i,2):
-  pagina = requests.get(("https://sistemas.anac.gov.br/certificacao/AvGeral/AIR145BasesDetail.asp?B145Codi=000000"+ str('%04d'%i)))
+    #separa as tabelas do arquivo
+    tables = soup.findAll('table', width="710")
 
-#transforma página em objeto
-soup = BeautifulSoup(pagina.text, 'lxml')
-#text = soup.get_text()
+    #pega apenas os campos da tabela que tem atributos de texto
+    tabela_basica = tables[3].findAll('font')
 
-#separa as tabelas do arquivo
-tables = soup.findAll('table', width="710")
+    #limpa as tags da tabela
+    for tag in tabela_basica: 
+      tag.attrs = None
 
+    print (len(tabela_basica)) #teste para verificar a quantidade de itens na tabela
+    
+    #imprime a posição e cada item da posição na tabela
+    nova_tabela = []
+    for i in tabela_basica:
+      nova_tabela.append((i.get_text("'", strip=True)))
 
-#pega apenas os campos da tabela que tem atributos de texto
-tabela_basica = tables[3].findAll('font')
+    #adiciona a lista na posição do dicionário de acordo com o número do endereço do fornecedor
+    dicio_fornecedores[x] = nova_tabela
+    x+=1
+    #print (tabela_basica)
+    print ("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
+  pprint.pprint(dicio_fornecedores)
 
-
-#limpa as tags da tabela
-for tag in tabela_basica: 
-  tag.attrs = None
-
-
-print (len(tabela_basica)) #teste para verificar a quantidade de itens na tabela
-
-print ("///////////////////////////////") #divisor
-
-#imprime a posição e cada item da posição na tabela
-nova_tabela = []
-for i in tabela_basica:
-  texto = [i.text]
-  nova_tabela.append((i.get_text("'", strip=True)))
-
-print (nova_tabela)
-  
-dicio_fornecedores = { i : nova_tabela[i] for i in range(0, len(nova_tabela) ) }
-#print (tabela_basica)
-print ("///////////////////////////////")
-print (dicio_fornecedores)
+webscraper()
